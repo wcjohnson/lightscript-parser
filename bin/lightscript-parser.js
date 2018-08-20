51cc98cda296e640b254dab32188cb9439e3e78a
William C. Johnson <wcjohnson@oigroup.net>
2018-08-20 01:27:35 -0700

    test: Add tools for debugging fixtures interactively

---
+++ bin/lightscript-parser.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+/* eslint no-var: 0 */
+
+var parser = require("..");
+var fs = require("fs");
+var path = require("path");
+
+var filename = process.argv[2];
+if (!filename) {
+  console.error("no filename specified");
+  process.exit(0);
+}
+if (filename === "fixture") {
+  filename = path.join("test", "fixtures", process.argv[3])
+  if (fs.existsSync(path.join(filename, "input.js"))) {
+    filename = path.join(filename, "input.js");
+  } else {
+    filename = path.join(filename, "input.lsc");
+  }
+}
+
+var mgr = new parser.PluginManager();
+mgr.addPlugins([
+  "additionalExtensionPoints", "disableBitwiseOperators", "tildeCall", "significantWhitespace", "modifiedLogicalOperators", "seqExprRequiresParen", "lscCoreSyntax", "jsx", "flow"
+]);
+
+var file = fs.readFileSync(filename, "utf8");
+var ast = parser.parse(file, {
+  pluginManager: mgr,
+  sourceType: "module"
+});
+
+console.log(JSON.stringify(ast, null, "  "));
